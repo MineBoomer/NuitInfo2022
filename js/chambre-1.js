420603677b7c1a39e91afb728da189632f627194
MineBoomer <77445779+MineBoomer@users.noreply.github.com>
2022-12-02 05:46:05 +0100

    js

---
+++ js/chambre-1.js
@@ -0,0 +1,81 @@
+//a : session chambre1
+//b : session cours
+//c : session épicerie
+
+
+function a1(){
+    var button = document.getElementById("self");
+    var newButton = document.createElement("a");
+    newButton.setAttribute("onclick", "a2()"); //Prochaine fonction
+    newButton.setAttribute("class", "a-low-button");
+    newButton.setAttribute("id", "self");
+    newButton.innerHTML = "Suivant"; //Texte bouton 2
+    var parentButton = button.parentElement;
+    parentButton.removeChild(button);
+    parentButton.appendChild(newButton);
+
+    var button2 = document.getElementById("self2");
+    var newButton2 = document.createElement("a");
+    newButton2.setAttribute("onclick", "");
+    newButton2.setAttribute("class", "a-low-button");
+    newButton2.setAttribute("id", "self2");
+
+    newButton2.innerHTML = ""; //Texte bouton 1
+
+    var parentButton = button2.parentElement;
+    parentButton.removeChild(button2);
+    parentButton.appendChild(newButton2);
+
+    var p = document.getElementById("text");
+    p.innerHTML = "Je m'ennuie"; //Texte de l'histoire
+}
+
+function a2(){
+    var button = document.getElementById("self");
+    var newButton = document.createElement("a");
+    newButton.setAttribute("onclick", "a3()");
+    newButton.setAttribute("class", "a-low-button");
+    newButton.setAttribute("id", "self");
+    newButton.innerHTML = "Suivant";
+    var parentButton = button.parentElement;
+    parentButton.removeChild(button);
+    parentButton.appendChild(newButton);
+
+    var button2 = document.getElementById("self2");
+    var newButton2 = document.createElement("a");
+    newButton2.setAttribute("onclick", "");
+    newButton2.setAttribute("class", "a-low-button");
+    newButton2.setAttribute("id", "self2");
+    newButton2.innerHTML = "";
+    var parentButton = button2.parentElement;
+    parentButton.removeChild(button2);
+    parentButton.appendChild(newButton2);
+
+    var p = document.getElementById("text");
+    p.innerHTML = "Mince j'ai cours !!!";
+}
+
+function a3(){
+    var button = document.getElementById("self");
+    var newButton = document.createElement("a");
+    newButton.setAttribute("onclick", "c1()");
+    newButton.setAttribute("class", "a-low-button");
+    newButton.setAttribute("id", "self");
+    newButton.innerHTML = "Non";
+    var parentButton = button.parentElement;
+    parentButton.removeChild(button);
+    parentButton.appendChild(newButton);
+
+    var button2 = document.getElementById("self2");
+    var newButton2 = document.createElement("a");
+    newButton2.setAttribute("onclick", "b1()");
+    newButton2.setAttribute("class", "a-low-button");
+    newButton2.setAttribute("id", "self2");
+    newButton2.innerHTML = "Oui";
+    var parentButton = button2.parentElement;
+    parentButton.removeChild(button2);
+    parentButton.appendChild(newButton2);
+
+    var p = document.getElementById("text");
+    p.innerHTML = "Est-ce que j'ai vraiment envie d'y aller ?";
+}
